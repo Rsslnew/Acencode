caf22da38aca7982206c046171417441e140efe6
Rsslnew <lexaxipa@gmail.com>
2026-05-30 06:23:44 +0700

    fix

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -1,21 +1,3 @@
-from .queue_manager import QueueManager
-from .encode import EncodeHandler
-from .progress import ProgressTracker
-from .cancel import CancelHandler
-from .callback import CallbackHandler
-from .settings import SettingsHandler
-from .text_input import TextInputHandler
-from .test_handler import test_all_messages, test_video_filter, test_command
-
-__all__ = [
-    "QueueManager",
-    "EncodeHandler",
-    "ProgressTracker",
-    "CancelHandler",
-    "CallbackHandler",
-    "SettingsHandler",
-    "TextInputHandler",
-    "test_all_messages",
-    "test_video_filter",
-    "test_command",
-]
+# handlers package
+# All handlers auto-register via @Client.on_message / @Client.on_callback_query
+# when their modules are imported in bot/__main__.py
